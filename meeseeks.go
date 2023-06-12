409ae6b7306e6c69566bfdb887e6fb3b3d30f2bc
Gwali <gwalisam37@gmail.com>
2023-06-12 16:32:39 +0000

    added route  matching function

--- meeseeks.go
+++ meeseeks.go
@@ -6,8 +6,9 @@ import (
 	"context"
 )
 
-//struct http.Handler
 
+
+// struct http.Handler
 type serverMux struct {
 	NotFound         http.Handler
 	MethodNotAllowed http.Handler
@@ -22,6 +23,7 @@ type route struct {
 	handler http.Handler
 }
 
+// initialize a new meeseeks router
 func NewMeeseeks() *serverMux {
 	return &serverMux{
 		NotFound: http.NotFoundHandler(),
@@ -35,6 +37,7 @@ func NewMeeseeks() *serverMux {
 //methods on it
 
 // method to register handlers on patterns
+// serverMux struct methods
 func (s *serverMux) GET(pattern string, handler http.HandlerFunc) {
 	allowedMethods := []string{http.MethodGet, http.MethodHead}
 	for _, method := range allowedMethods {
@@ -76,8 +79,51 @@ func (r route) match(url string, c context.ContextC){
 }
 
 //extract path parameter value
+// middleware function must have http.HandlerFunc function signature thus func(http.ResponseWriter, *http.Request)
+func (s *serverMux) Use(m ...func(http.HandlerFunc) http.HandlerFunc) {
+	s.middlewares = append(s.middlewares, m...)
+
+}
+
+//method serverHttp is implemented by serverMux struct making it a http.Handler
+
+// match function
+func (r route) match(c context.Context, requestURL string) (context.Context, bool) {
+	urlPathSegements := strings.Split(requestURL, "/")
+	if len(r.path) != len(urlPathSegements) {
+		return c, false
+	}
+
+	for i, segement := range r.path {
+		if strings.HasPrefix(segement, ":") {
+			if urlPathSegements[i] != "" {
+				key := strings.Split(segement, ":")[1]
+				c = context.WithValue(c, string(key), urlPathSegements[i])
+				continue
+			}
+			return c, false
+		}
+
+		if segement != urlPathSegements[i] {
+			return c, false
+		}
+	}
+
+	return c, true
+}
+
+// extract path parameter value
+func loadParam(c context.Context, paramName string) string {
+	value, ok := c.Value(string(paramName)).(string) //type assertion
+	//if value is not of type string
+	if !ok {
+		return ""
+	}
+	return value
+}
 
 //custom
 //middleware to server files from a directory
 //middleware for jwt handling
 //middleware protected routes by specifying middleware name
+// /home/:name/love   /home//love    /home/marco/love
