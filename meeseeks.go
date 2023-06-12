7f07d16da64ed5c916c94202788678585d9c6de6
Gwali <gwalisam37@gmail.com>
2023-06-12 14:31:42 +0000

    added methods get, post and wrap to serverMux struct.

--- meeseeks.go
+++ meeseeks.go
@@ -1,26 +1,68 @@
 package meeseeks
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+)
 
 //struct http.Handler
 
 type serverMux struct {
 	NotFound         http.Handler
 	MethodNotAllowed http.Handler
-	registeredRoutes []*routes
+	registeredRoutes *[]route
 	middlewares      []func(http.HandlerFunc) http.HandlerFunc
 }
 
 // struct to hold route handler info
-type routes struct {
+type route struct {
 	method  string
-	path    string
+	path    []string
 	handler http.Handler
 }
 
 //methods on it
 
 // method to register handlers on patterns
+
+func (s *serverMux) GET(pattern string, handler http.HandlerFunc) {
+	allowedMethods := []string{http.MethodGet, http.MethodHead}
+	for _, method := range allowedMethods {
+		newRoute := route{
+			method:  method,
+			path:    strings.Split(pattern, "/"),
+			handler: http.HandlerFunc(s.wrap(handler)),
+		}
+
+		*s.registeredRoutes = append(*s.registeredRoutes, newRoute)
+	}
+}
+
+func (s *serverMux) POST(pattern string, handler http.HandlerFunc) {
+	allowedMethods := []string{http.MethodPost}
+	for _, method := range allowedMethods {
+		newRoute := route{
+			method:  method,
+			path:    strings.Split(pattern, "/"),
+			handler: http.HandlerFunc(handler),
+		}
+
+		*s.registeredRoutes = append(*s.registeredRoutes, newRoute)
+	}
+}
+
+
+
+
+func (s *serverMux) wrap(handler http.HandlerFunc)http.HandlerFunc{
+	for _,m := range s.middlewares{
+		handler = m(handler)
+	}
+	return handler
+}
+
+
+
 
 //method to wrap middlewares around handlers
 
