debb972b5dd2e37a80074ac1bb91af8c531a1a48
Gwali <gwalisam37@gmail.com>
2023-06-13 12:17:04 +0000

    added a few comment notes

--- methods.go
+++ methods.go
@@ -5,14 +5,12 @@ import (
 	"strings"
 )
 
-
 //this file contains all methods on the serveMux struct used to register patterns
 //with handlers
 //they are specified with http verbs indicating the request method to be used in the request
 //fucntions with http.HandlerFunc type are accepted as the argument to be used as request handler for conveninece. They are converted to handlers later on
 //the whole router interface is designed to work with functions of type http.HandlerFunc from the users end
 
-// serverMux struct methods
 func (s *serverMux) GET(pattern string, handler http.HandlerFunc) {
 	allowedMethods := []string{http.MethodGet, http.MethodHead}
 	for _, method := range allowedMethods {
